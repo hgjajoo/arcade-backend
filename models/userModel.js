5299f38bda7e6c7cbfe4cbf706c5ff46102c22f2
foundRamit <tapariaramitr@gmail.com>
2025-01-23 01:31:59 +0530

    hased password added

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,23 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   fullname: { type: String, required: true },
-//   email: { type: String, required: true },
-//   password: { type: String, required: true },
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 });
 
-module.exports = mongoose.model("User", userSchema);
+// Pre-save hook to hash the password
+userSchema.pre('save', async function (next) {
+  // Hash the password only if it’s new or modified
+  if (!this.isModified('password')) return next();
+  
+  // Hash the password with bcrypt
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Create and export the User model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
+
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");
 const authController = require("../controllers/authController"); // Adjusted path
 
